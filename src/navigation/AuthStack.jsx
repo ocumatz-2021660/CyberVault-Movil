35d5dd4bad8488861324876cc632aad42377fe74
jmagzul-2024029 <jmagzul-2024029@kinal.edu.gt>
2026-06-18 21:56:49 -0600

    feat: agregando movilidad entre paginas

--- src/navigation/AuthStack.jsx
+++ src/navigation/AuthStack.jsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator} from "@react-navigation/native-stack"
 import LoginScreen from "../features/auth/screens/LoginScreen"
 import RegisterScreen from "../features/auth/screens/RegisterScreen"
+import ForgotPasswordScreen from "../features/auth/screens/ForgotPasswordScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const AuthStack = ()=>{
         >
             <Stack.Screen name= "Login" component={LoginScreen}/>            
             <Stack.Screen name= "Register" component={RegisterScreen}/>
+            <Stack.Screen name= "ForgotPassword" component={ForgotPasswordScreen}/>
         </Stack.Navigator>
     )
 }
