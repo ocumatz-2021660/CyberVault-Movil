afbef18f92e9c6e9fe40e9190327ed80f9f51701
jmagzul-2024029 <jmagzul-2024029@kinal.edu.gt>
2026-06-26 22:50:54 -0600

    feat: agregando logica para implementación de usuarios

---
+++ src/shared/store/authStore.js
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const useAuthStore = create(
+    persist(
+        (set) => ({
+            token: null,
+            user: null,
+            isAuthenticated: false,
+            _hasHydrated: false,
+
+            setHasHydrated: (state) => set({ _hasHydrated: state }),
+
+            login: async (accessToken, user) => {
+                set({
+                    token: accessToken,
+                    user,
+                    isAuthenticated: true,
+                });
+            },
+
+            setAccessToken: (token) => set({ token }),
+
+            logout: async () => {
+                set({
+                    token: null,
+                    user: null,
+                    isAuthenticated: false,
+                });
+            },
+        }),
+        {
+            name: "auth-storage",
+            storage: createJSONStorage(() => AsyncStorage),
+            onRehydrateStorage: () => (state) => {
+                state?.setHasHydrated(true);
+            },
+        },
+    ),
+);
