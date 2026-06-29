364153210148637c53c2cf39e40fbab60e0b73c5
cchacon-2021560 <cchacon-2021560@kinal.edu.gt>
2026-06-28 18:33:56 -0600

    feat: definir lista de monedas disponibles

--- src/shared/store/settingStore.js
+++ src/shared/store/settingStore.js
@@ -1,3 +1,12 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const AVAILABLE_CURRENCIES = [
+    { code: "GTQ", symbol: "Q", name: "Quetzal Guatemalteco" },
+    { code: "USD", symbol: "$", name: "Dólar" },
+    { code: "EUR", symbol: "€", name: "Euro" },
+    { code: "GBP", symbol: "£", name: "Libra" },
+    { code: "MXN", symbol: "MX$", name: "Peso Mexicano" },
+    { code: "COP", symbol: "COL$", name: "Peso Colombiano" },
+];
