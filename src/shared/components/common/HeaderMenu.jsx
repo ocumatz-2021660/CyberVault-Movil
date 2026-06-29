dfe95c114cc5744d070bd016f9be581d01d70dd2
cchacon-2021560 <cchacon-2021560@kinal.edu.gt>
2026-06-28 18:53:09 -0600

    feat: crear estructura base y animaciones de apertura/cierre del menú

---
+++ src/shared/components/common/HeaderMenu.jsx
@@ -0,0 +1,50 @@
+import React, { useRef, useEffect, useState } from "react";
+import {View, Text,TouchableOpacity,TouchableWithoutFeedback,  StyleSheet, Animated, Dimensions, Modal,} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { COLORS, SPACING, FONT_SIZE, SHADOWS } from "../../constants/theme";
+import { useAuth } from "../../../features/auth/hooks/useAuth";
+import { useSettingsStore, AVAILABLE_CURRENCIES } from "../../store/settingStore";
+import { User, DollarSign, ChevronRight, LogOut,ArrowLeft, CircleDot, Circle,} from "lucide-react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const POPOVER_WIDTH = SCREEN_WIDTH * 0.55;
+
+const HeaderMenu = ({ visible, onClose, navigation }) => {
+    const scale = useRef(new Animated.Value(0)).current;
+    const opacity = useRef(new Animated.Value(0)).current;
+    const { logout } = useAuth();
+    const { currency, setCurrency } = useSettingsStore();
+    const [showCurrencies, setShowCurrencies] = useState(false);
+
+    useEffect(() => {
+        if (visible) {
+            setShowCurrencies(false);
+            Animated.parallel([
+                Animated.spring(scale, {
+                    toValue: 1,
+                    useNativeDriver: true,
+                    friction: 8,
+                }),
+                Animated.timing(opacity, {
+                    toValue: 1,
+                    duration: 150,
+                    useNativeDriver: true,
+                }),
+            ]).start();
+        } else {
+            Animated.parallel([
+                Animated.timing(scale, {
+                    toValue: 0,
+                    duration: 100,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(opacity, {
+                    toValue: 0,
+                    duration: 100,
+                    useNativeDriver: true,
+                }),
+            ]).start();
+        }
+    }, [visible]);
+
+}
