7235db1b7280fff7f4719ec6abc88bd42da702d8
ocumatz-2021660 <ocumatz-2021660@kinal.edu.gt>
2026-06-29 20:27:07 -0600

    fix: corrigiendo orden de Tabs navegation

--- src/navigation/MainTabs.jsx
+++ src/navigation/MainTabs.jsx
@@ -94,15 +94,15 @@ const MainTabs = () => {
                     paddingTop: 5,
                 },
                 tabBarIcon: ({ color, size }) => {
-                    if (route.name === "Canjes") return <TicketCheckIcon size={size} color={color} />;
-                    else if (route.name === "Home") return <HomeIcon size={size} color={color} />;
+                    if (route.name === "Home") return <HomeIcon size={size} color={color} />;
+                    else if (route.name === "Canje") return <TicketCheckIcon size={size} color={color} />;
                     else if (route.name === "Service") return <GiftIcon size={size} color={color} />;
                     else if (route.name === "Favorite") return <Heart size={size} color={color} />;
                 },
             })}
         >
-            <Tab.Screen name="Canjes" component={AccountStack} options={{ tabBarLabel: "Canjes" }} />
             <Tab.Screen name="Home" component={AccountStack} options={{ tabBarLabel: "Inicio" }} />
+            <Tab.Screen name="Canje" component={AccountStack} options={{ tabBarLabel: "Canjes" }} />
             <Tab.Screen name="Service" component={AccountStack} options={{ tabBarLabel: "Servicios" }} />
             <Tab.Screen name="Favorite" component={AccountStack} options={{ tabBarLabel: "Favoritos" }} />
         </Tab.Navigator>
