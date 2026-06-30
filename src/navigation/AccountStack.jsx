8b7cee96d92a26e6796ccce6d38e57ee799b5454
jmendez-2021550 <jmendez-2021550@kinal.edu.gt>
2026-06-29 22:32:10 -0600

    feat: agregando ruta de navegación para Transacciones

--- src/navigation/AccountStack.jsx
+++ src/navigation/AccountStack.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import DashboardAccountScreen from "../features/account/screens/DashboardAccountScreen";
 import CreateAccountScreen from "../features/account/screens/CreateAccountScreen";
+import TransactionScreen from "../features/transaction/screens/TransactionScreen"
+import WithdrawalScreen from "../features/withdrawal/screens/WithdrawalScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +12,8 @@ const AccountStack = () => {
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="DashboardAccount" component={DashboardAccountScreen} />
             <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
+            <Stack.Screen name="Transaction" component={TransactionScreen} />
+
         </Stack.Navigator>
     );
 };
